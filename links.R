02c7b97852faf5cf4eff1dcad322cf41a6057a2e
Michael France Nelson <michaelfrancenelson@gmail.com>
2018-05-14 09:28:51 -0400

    Added some examples of facets/panels for same and different data sets. Added a file with links.

---
+++ links.R
@@ -0,0 +1,2 @@
+# A ton of example plots with code:
+# http://r-statistics.co/Top50-Ggplot2-Visualizations-MasterList-R-Code.html 
--- panels_facets.R
+++ panels_facets.R
@@ -1,7 +1,8 @@
 require(ggplot2)
-# (temporal) Autocorrelation in mean red-stage pines in the study forests
+require(gridExtra)
 
-i = 1:4
+# Facets from same data set -----------------------------------------------
+
 dat1 = data.frame(
   forest = rep(c("for1", "for2", "for3", "for4"), each = 20),
   x = rep(1:20, 4),
@@ -15,4 +16,26 @@ dat1 = data.frame(
 gg1 = ggplot(data = dat1, aes(x = x, y = y, color = forest))
 f1 = facet_wrap(~forest, ncol = 2)
 gg1 + f1 + geom_bar(stat = "identity", aes(fill = forest), color = 1, width = 1)
+
+# with different scales in each facet
+f2 = facet_wrap (~ forest, ncol = 2, scales = "free")
+gg1 + f2 + geom_bar(stat = "identity", aes(fill = forest), color = 1, width = 1)
+
+
+
+
+# facets from different data sets -----------------------------------------
+
+# use grid.arrange() from gridExtra
+# https://cran.r-project.org/web/packages/egg/vignettes/Ecosystem.html
+
+d1 = data.frame(x = rnorm(100), y = rnorm(100))
+d2 = data.frame(x = 1:100, obs = factor(rpois(100, lambda = 5)))
+d3 = data.frame(x = seq(0, 1, length.out = 100), y = rexp(n = 100, 1.5) + seq(0, 1, length.out = 100))
+
+g1 = ggplot(d1, aes(x, y, color = y)) + geom_point(); g1
+g2 = ggplot(d2, aes(x = obs)) + geom_bar(stat = "count"); g2
+g3 = ggplot(d3, aes(x, y)) + geom_line(); g3
+
+grid.arrange(g1, g2, g3, nrow = 1)
 
